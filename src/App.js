920ecec3bf635737a6717f73708bd6d3076ec017
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2021-02-15 00:28:25 -0800

    added gdg links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
 import React from "react";
 
+// Components
 import NavBar from "./components/NavBar/NavBar";
 import Routes from "./components/Routes";
+
+// Styles
+import { InstagramIconStyle } from "./styles";
+import { FaInstagramSquare } from "react-icons/fa";
+import { IoLogoFacebook } from "react-icons/io";
+import { FaTwitterSquare } from "react-icons/fa";
+import { IoLogoLinkedin } from "react-icons/io";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Routes />
+      <InstagramIconStyle className="row">
+        <div className="col-12 col-md-2 col-lg-2">
+          <p>Follow us on</p>
+          <a
+            href="https://instagram.com/gdgnajaf?igshid=1xmmrpow601ga"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaInstagramSquare color="grey" size="20" />
+          </a>
+          <a
+            href="/https://www.facebook.com/najafgdg/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <IoLogoFacebook color="grey" size="24" />
+          </a>
+          <a
+            href="/https://twitter.com/gdgnajaf?lang=ar"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaTwitterSquare color="grey" size="21.5" />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/gdg-najaf/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <IoLogoLinkedin color="grey" size="24" />
+          </a>
+        </div>
+        <div className="col-12 col-md-10 col-lg-10">
+          <p>Join us on our GDG platform to reach all our upcoming events 👇🏼</p>
+          <a
+            href="gdg.community.dev/gdg-najaf"
+            target="_blank"
+            rel="noreferrer"
+          >
+            gdg.community.dev/gdg-najaf
+          </a>
+        </div>
+      </InstagramIconStyle>
     </div>
   );
 }
---
+++ src/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const InstagramIconStyle = styled.div`
+  border-top: 1px solid #979b97;
+  /* justify-content: center; */
+  /* display: flex; */
+  /* flex-wrap: wrap; */
+  margin: 10em 5em 0em 5em;
+  padding: 20px;
+  p {
+    display: block;
+    color: grey;
+    font-size: 15px;
+    padding-top: 1em;
+  }
+  div {
+    /* display: inline-flex; */
+  }
+`;
